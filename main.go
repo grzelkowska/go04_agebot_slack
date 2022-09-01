3990e56cc08df7a2c6223fcb161b57adf1c65ac1
Phillip <philliplee515@gmail.com>
2022-09-01 16:52:43 +0900

    final commit

--- main.go
+++ main.go
@@ -1,22 +1,64 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
-	// "github.com/shomali11/slacker"
+	"github.com/shomali11/slacker"
 )
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
 	// godotenv
 	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Println("Could not load .env file")
+		fmt.Println("Could not load .env file", err)
 	}
 	SLACK_BOT_TOKEN := os.Getenv("SLACK_BOT_TOKEN")
 	SLACK_APP_TOKEN := os.Getenv("SLACK_APP_TOKEN")
 
 	os.Setenv("SLACK_BOT_TOKEN", SLACK_BOT_TOKEN)
 	os.Setenv("SLACK_APP_TOKEN", SLACK_APP_TOKEN)
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My YoB is <year>", &slacker.CommandDefinition{
+		Description: "YoB Calculator",
+		Examples:    []string{"My YoB is 2020"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				fmt.Println("error")
+			}
+			age := 2021 - yob
+			r := fmt.Sprintf("Age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
